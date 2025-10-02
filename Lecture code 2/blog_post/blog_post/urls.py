5a261868a425bc84ff083c0cf60fa2f393267831
MariamKipshidze <mari.kifshidze@gmail.com>
2025-10-02 21:14:19 +0400

    Create path('api-auth/', include('rest_framework.urls')),

--- Lecture code 2/blog_post/blog_post/urls.py
+++ Lecture code 2/blog_post/blog_post/urls.py
@@ -21,6 +21,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api-auth/', include('rest_framework.urls')),
     path('blog/', include('blog.urls')),
     path('user/', include('user.urls'))
 ]
